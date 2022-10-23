d3a9dc039e5c948b670407943d5791555e10d6ef
nono2224 <88888931+nono2224@users.noreply.github.com>
2022-10-23 17:33:16 +0900

    change: indexのスタイル変更
    index.phpのスタイルの作成

--- public/index.php
+++ public/index.php
@@ -17,15 +17,152 @@ if($result) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/index/main.css">
   <title>大喜利</title>
 </head>
 <body>
 
-  <a href="mypage.php">マイページへ</a>
-  <a href="signup_form.php">登録はこちら</a>
-  <a href="login_form.php">ログインはこちら</a>
-  <a href="item.php">項目</a>
-  <a href="odai.php">投稿です</a>
+<header>
+        <div class="header-top">
+            <div class="header-top-content">
+                <a class="header-top-content-home"></a>
+                <a href="mypage.php" class="header-top-content-account"></a>
+                <a href="item.php" class="header-top-content-item"></a>
+            </div>
+        </div>
+        <div class="header-bottom">
+            <div class="header-bottom-content">
+                <a href="signup_form.php" class="header-bottom-register"></a>
+                <a href="odai.php" class="header-bottom-content-post"></a>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="main-content">
+            <div class="main-content-content">
+                <div class="main-content-content-name">
+                    <div class="main-content-content-name-text">AAA</div>
+                </div>
+                <div class="main-content-content-posts">
+                    <div class="main-content-content-posts-area">
+                        <div class="main-content-content-posts-area-post">
+                            <div class="main-content-content-posts-area-post-content">
+                                <div class="main-content-content-posts-area-post-content-text">あああああああああああ</div>
+                            </div>
+                            <div class="main-content-content-posts-area-post-meta">
+                                <div class="main-content-content-posts-area-post-meta-name">
+                                    <div class="main-content-content-posts-area-post-meta-name-text">名前</div>
+                                </div>
+                                <div class="main-content-content-posts-area-post-meta-data">
+                                    <div class="main-content-content-posts-area-post-meta-data-text">2022-01-01</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="main-content-content-posts-area-post">
+                            <div class="main-content-content-posts-area-post-content">
+                                <div class="main-content-content-posts-area-post-content-text">あああああああああああ</div>
+                            </div>
+                            <div class="main-content-content-posts-area-post-meta">
+                                <div class="main-content-content-posts-area-post-meta-name">
+                                    <div class="main-content-content-posts-area-post-meta-name-text">名前</div>
+                                </div>
+                                <div class="main-content-content-posts-area-post-meta-data">
+                                    <div class="main-content-content-posts-area-post-meta-data-text">2022-01-01</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="main-content-content-posts-area-post">
+                            <div class="main-content-content-posts-area-post-content">
+                                <div class="main-content-content-posts-area-post-content-text">あああああああああああ</div>
+                            </div>
+                            <div class="main-content-content-posts-area-post-meta">
+                                <div class="main-content-content-posts-area-post-meta-name">
+                                    <div class="main-content-content-posts-area-post-meta-name-text">名前</div>
+                                </div>
+                                <div class="main-content-content-posts-area-post-meta-data">
+                                    <div class="main-content-content-posts-area-post-meta-data-text">2022-01-01</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="main-content-content-posts-area-post">
+                            <div class="main-content-content-posts-area-post-content">
+                                <div class="main-content-content-posts-area-post-content-text">あああああああああああ</div>
+                            </div>
+                            <div class="main-content-content-posts-area-post-meta">
+                                <div class="main-content-content-posts-area-post-meta-name">
+                                    <div class="main-content-content-posts-area-post-meta-name-text">名前</div>
+                                </div>
+                                <div class="main-content-content-posts-area-post-meta-data">
+                                    <div class="main-content-content-posts-area-post-meta-data-text">2022-01-01</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+            <div class="main-content-content">
+                <div class="main-content-content-name">
+                    <div class="main-content-content-name-text">AAA</div>
+                </div>
+                <div class="main-content-content-posts">
+                    <div class="main-content-content-posts-area">
+                        <div class="main-content-content-posts-area-post">
+                            <div class="main-content-content-posts-area-post-content">
+                                <div class="main-content-content-posts-area-post-content-text">あああああああああああ</div>
+                            </div>
+                            <div class="main-content-content-posts-area-post-meta">
+                                <div class="main-content-content-posts-area-post-meta-name">
+                                    <div class="main-content-content-posts-area-post-meta-name-text">名前</div>
+                                </div>
+                                <div class="main-content-content-posts-area-post-meta-data">
+                                    <div class="main-content-content-posts-area-post-meta-data-text">2022-01-01</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="main-content-content-posts-area-post">
+                            <div class="main-content-content-posts-area-post-content">
+                                <div class="main-content-content-posts-area-post-content-text">あああああああああああ</div>
+                            </div>
+                            <div class="main-content-content-posts-area-post-meta">
+                                <div class="main-content-content-posts-area-post-meta-name">
+                                    <div class="main-content-content-posts-area-post-meta-name-text">名前</div>
+                                </div>
+                                <div class="main-content-content-posts-area-post-meta-data">
+                                    <div class="main-content-content-posts-area-post-meta-data-text">2022-01-01</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="main-content-content-posts-area-post">
+                            <div class="main-content-content-posts-area-post-content">
+                                <div class="main-content-content-posts-area-post-content-text">あああああああああああ</div>
+                            </div>
+                            <div class="main-content-content-posts-area-post-meta">
+                                <div class="main-content-content-posts-area-post-meta-name">
+                                    <div class="main-content-content-posts-area-post-meta-name-text">名前</div>
+                                </div>
+                                <div class="main-content-content-posts-area-post-meta-data">
+                                    <div class="main-content-content-posts-area-post-meta-data-text">2022-01-01</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="main-content-content-posts-area-post">
+                            <div class="main-content-content-posts-area-post-content">
+                                <div class="main-content-content-posts-area-post-content-text">あああああああああああ</div>
+                            </div>
+                            <div class="main-content-content-posts-area-post-meta">
+                                <div class="main-content-content-posts-area-post-meta-name">
+                                    <div class="main-content-content-posts-area-post-meta-name-text">名前</div>
+                                </div>
+                                <div class="main-content-content-posts-area-post-meta-data">
+                                    <div class="main-content-content-posts-area-post-meta-data-text">2022-01-01</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
