303a34859d08e8fbad8220ec251863d6874029a3
nono2224 <88888931+nono2224@users.noreply.github.com>
2022-10-26 11:56:35 +0900

    add: jqueryの追加

--- public/index.php
+++ public/index.php
@@ -57,6 +57,7 @@ $post_array = $pdo->query($sql);
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../css/index/main.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
   <title>大喜利</title>
 </head>
 <body>
