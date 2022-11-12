a8c727c0459167c8e8882132697d3b8cd947af7d
nono2224 <88888931+nono2224@users.noreply.github.com>
2022-11-12 15:03:15 +0900

    update: ポップアップにより投稿ページが表示出来るように

---
+++ script/index/index.js
@@ -0,0 +1,7 @@
+$(".header-bottom-content-post").click(function() {
+    $(".postLayer").css("display", "block");
+});
+
+$(".form-top-top-closeButton").click(function() {
+    $(".postLayer").css("display", "none");
+});
