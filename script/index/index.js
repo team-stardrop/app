cc716fe4fddf61bffd06caca7591d31ab66ccfa5
nono2224 <88888931+nono2224@users.noreply.github.com>
2022-11-12 19:25:42 +0900

    change: index内の投稿ポップアップの閉じる方法の種類を追加
    index内の投稿ポップアップの閉じる方法として、「グレー色のバックグラウンドをクリックで閉じる」と「escキーで閉じる」と追加

--- script/index/index.js
+++ script/index/index.js
@@ -1,7 +1,29 @@
+$(window).keydown(function(e) {
+    if (e.keyCode == 27) {
+        postLayer_close();
+    }
+});
+
 $(".header-bottom-content-post").click(function() {
-    $(".postLayer").css("display", "block");
+    postLayer_open();
 });
 
 $(".form-top-top-closeButton").click(function() {
-    $(".postLayer").css("display", "none");
+    postLayer_close();
 });
+
+$(".postLayer").click(function() {
+    postLayer_close();
+});
+
+
+
+function postLayer_open() {
+    $(".postLayer").css("display", "block");
+    $(".postLayer-content").css("display", "block");
+}
+
+function postLayer_close() {
+    $(".postLayer").css("display", "none");
+    $(".postLayer-content").css("display", "none");
+}
