77a3485fb442014c8adbf92a5ead916852ef00a0
k21067kk <63432884+koron56@users.noreply.github.com>
2022-11-13 11:31:46 +0900

    ユーザ登録画面の変更
    signup_form.phpの23行目のcssの読み込みURLの変更
    login_form/main.css → signup_form/main.css
    
    スクロールバーの追加

--- public/signup_form.php
+++ public/signup_form.php
@@ -20,7 +20,7 @@ unset($_SESSION['login_err']);
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../css/login_form/main.css">
+        <link rel="stylesheet" href="../css/signup_form/main.css">
         <title>ユーザ登録画面</title>
     </head>
     <body>
