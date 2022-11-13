6aae08c6c33d8cde5c0a44bd76383af09c86018a
k21067kk <63432884+koron56@users.noreply.github.com>
2022-11-13 21:30:27 +0900

    ログイン画面のデザインの作成
    ログイン画面のデザインを作成した。

--- public/login_form.php
+++ public/login_form.php
@@ -22,37 +22,47 @@ session_destroy();
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/login_form/main.css">
   <title>ログイン画面</title>
 </head>
 <body>
-  <h2>ログインフォーム</h2>
-  <!-- エラーメッセージ -->
-  <?php if (isset($err['msg'])) : ?>
-    <p><?php echo $err['msg']; ?></p>
-  <?php endif; ?>
-
-  <!-- 登録フォーム -->
-  <form action="login.php" method="POST">
-    <p>
-      <label for="username">メールアドレス：</label>
-      <input type="email" name="email">
-      <?php if (isset($err['email'])) : ?>
-          <p><?php echo $err['email']; ?></p>
-      <?php endif; ?>
-    </p>
-    <p>
-      <label for="password">パスワード：</label>
-      <input type="password" name="password">
-      <?php if (isset($err['password'])) : ?>
-          <p><?php echo $err['password']; ?></p>
-      <?php endif; ?>
-    </p>
-    <p>
-      <input type="submit" value="ログイン">
-    </p>
-    <a href="signup_form.php">新規登録はこちら</a>
-    <a href="index.php">戻る</a>
-  </form>
-
+    <div class="taggle-button-box">
+        <a href="index.php" class="home">
+            <div class="home-icon"></div>
+        </a>
+    </div>
+    <div class="login-form">
+        <div class="login-form-title-box">
+            <h2 class="login-form-title">ログインフォーム</h2>
+            <!-- エラーメッセージ -->
+            <?php if (isset($err['msg'])) : ?>
+                <p><?php echo $err['msg']; ?></p>
+            <?php endif; ?>
+        </div>
+        <!-- 登録フォーム -->
+        <div class="container-login">
+            <form action="login.php" method="POST">
+                <div class="input-field-group">
+                    <div class="input-field">
+                        <!--<label for="username">メールアドレス：</label>-->
+                        <input type="email" name="email" placeholder=メールアドレス>
+                    </div>
+                    <?php if (isset($err['email'])) : ?>
+                        <p class="error-message"><?php echo $err['email']; ?></p>
+                    <?php endif; ?>
+                    <div class="input-field-group">
+                        <div class="input-field">
+                        <!--<label for="password">パスワード：</label>-->
+                        <input type="password" name="password" placeholder=パスワード>
+                    </div>
+                    <?php if (isset($err['password'])) : ?>
+                        <p class="error-message"><?php echo $err['password']; ?></p>
+                    <?php endif; ?>
+                </div>
+                <input type="submit" value="ログイン" class="submit">
+            </form>
+        </div>
+        <a href="signup_form.php" class="login">新規登録はこちら</a>
+    </div>
 </body>
 </html>
