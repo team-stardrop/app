8147f393ec1cc761ee1b1e4ff1639eb945c28d97
nono2224 <88888931+nono2224@users.noreply.github.com>
2022-11-27 00:56:45 +0900

    add: odai.php内の編集及び削除ボタンの作成

--- public/odai.php
+++ public/odai.php
@@ -179,6 +179,14 @@ if ($result) {
             </div>
 
         </div>
+
+        <div class="slide">
+            <div class="slide-bar">
+                <div class="slide-bar-icon"></div>
+            </div>
+            <a class="slide-edit">編集する</a>
+            <a class="slide-delete">削除する</a>
+        </div>
         <!-- ここに書くよ！ -->
     </main>
 
@@ -229,6 +237,71 @@ if ($result) {
             <div class="form-top">
                 <div class="form-top-top">
                     <a class="form-top-top-closeButton">
+                        <div class="form-top-top-closeButton-border-1"></div>
+                        <div class="form-top-top-closeButton-border-2"></div>
+                    </a>
+                </div>
+                <div class="form-top-bottom">
+                    <div class="form-top-bottom-content">
+                        <textarea placeholder="お題を記入．．．" name="odai"></textarea>
+                    </div>
+                </div>
+            </div>
+            <div class="form-bottom">
+                <a class="form-bottom-postButtonContent">
+                    <input class="form-bottom-postButtonContent-text" type="submit" value="投稿する" name="submitButton">
+                    <input type="hidden" name="user_id" value="<?php echo $login_user['id'] ?>">
+                    <input type="hidden" name="post_date" value="<?php echo date("Y-m-d H:i:s") ?>">
+                </a>
+            </div>
+        </div>
+    </form>
+
+    <form class="postLayer-content-edit" method="POST">
+        <div class="category">
+            <ul class="category-content">
+                <li>
+                    <input type="radio" name="post-category" id="animal">
+                    <label for="animal">動物</label>
+                </li>
+                <li>
+                    <input type="radio" name="post-category" id="sport">
+                    <label for="sport">スポーツ</label>
+                </li>
+                <li><input type="radio" name="post-category" id="job">
+                    <label for="job">仕事</label>
+                </li>
+                <li>
+                    <input type="radio" name="post-category" id="school">
+                    <label for="school">学校</label>
+                </li>
+                <li>
+                    <input type="radio" name="post-category" id="food">
+                    <label for="food">食べ物</label>
+                </li>
+                <li>
+                    <input type="radio" name="post-category" id="trip">
+                    <label for="trip">旅行</label>
+                </li>
+                <li>
+                    <input type="radio" name="post-category" id="love">
+                    <label for="love">恋愛</label>
+                </li>
+                <li>
+                    <input type="radio" name="post-category" id="event">
+                    <label for="event">行事</label>
+                </li>
+                <li>
+                    <input type="radio" name="post-category" id="other">
+                    <label for="other">その他</label>
+                </li>
+            </ul>
+        </div>
+
+        <div class="form">
+            <div class="form-top">
+                <div class="form-top-top">
+                    <a class="form-top-top-closeButton-editver">
                         <div class="form-top-top-closeButton-border-1"></div>
                         <div class="form-top-top-closeButton-border-2"></div>
                     </a>
