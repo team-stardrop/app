d7714f3941d935b88b64e7f92482e4edc8c108de
nono2224 <88888931+nono2224@users.noreply.github.com>
2022-11-28 10:16:34 +0900

    add: コインの数を表示

--- public/index.php
+++ public/index.php
@@ -152,6 +152,16 @@ $post_array = $pdo->query($sql);
             <?php endif; ?>
         </div>
 
+
+        <div class="main-header">
+            <div class="main-header-text">
+                <h1>大喜利アプリ</h1>
+            </div>
+            <div class="main-header-coin">
+                <div class="main-header-coin-icon"></div>
+                <div class="main-header-coin-num">1234</div>
+            </div>
+        </div>
         <div class="main-content">
             <div class="main-content-content">
                 <div class="main-content-content-name">
